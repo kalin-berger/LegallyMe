af2d5ebe85b2278de1575a872e8164af220fc491
Thy <dfbarsell@gmail.com>
2019-05-04 20:06:37 -0400

    Beginning implementation of splash screen

--- app/src/main/java/com/github/crummish/legallyme/activity/MainActivity.java
+++ app/src/main/java/com/github/crummish/legallyme/activity/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_AppCompat_Light_NoActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //TODO: Implement custom toolbar for title centering
